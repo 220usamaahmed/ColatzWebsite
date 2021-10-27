e474afa5e51e92c381edad140469efac94069166
Usama Ahmed <220usamaahmed@gmail.com>
2021-10-27 14:32:38 +0500

    Initial commit

---
+++ components/AboutSection.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="about">
+    <div class="introduction">
+      <p>My name is Usama Ahmed Siddiquie (or colatz online).</p>
+      <p>
+        This site contains work I have done involving visual mediums. Scroll to
+        start viewing my projects.
+      </p>
+    </div>
+    <div class="contact">
+      <h1>CONTACT</h1>
+      <p><a href="#">sayhello@colatz.com</a></p>
+      <p><a href="#">220usamaahmed on LinkedIn</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function() {
+    const observer = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add('show');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+    
+    observer.observe(document.querySelector('.introduction'));
+    observer.observe(document.querySelector('.contact'));
+  },
+}
+</script>
+
+
+<style scoped lang="scss">
+.about {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  padding: 120px 0;
+
+  // background-color: pink;
+
+  .introduction {
+    grid-column: 2 / 6;
+    opacity: 0;
+    transition: opacity 240ms ease-in;
+
+    p {
+      color: black;
+      margin-bottom: 12px;
+      line-height: 1.8rem;
+    }
+
+    p:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .contact {
+    grid-column: 10 / -1;
+    opacity: 0;
+    transition: opacity 240ms ease-in;
+    transition-delay: 240ms;
+
+    h1 {
+      color: #666666;
+      font-size: 1rem;
+      font-weight: 400;
+      margin-bottom: 12px;
+    }
+
+    p {
+      margin-bottom: 12px;
+      letter-spacing: 1px;
+
+      a {
+        text-decoration: underline;
+        color: black;
+      }
+    }
+  }
+
+  .show {
+    opacity: 1;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .about {
+    padding: 60px 24px;
+
+    .introduction {
+      grid-column: 1 / -1;
+
+    }
+
+    .contact {
+      display: none;
+      margin-top: 60px;
+      grid-column: 1 / -1;
+    }
+  }
+}
+</style>
---
+++ content/project-02-test-project.md
@@ -0,0 +1,37 @@
+---
+title: Emergence
+description: >
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nascetur nunc non sit cras duis scelerisque etiam adipiscing. Enim elit id sed sed molestie augue faucibus. 
+
+
+  Aliquam mi nisi sed vel vitae eu, at nisl. Volutpat enim maecenas euismod turpis condimentum.
+tools_used: Processing, Adobe Premier and Lightroom
+---
+
+![Frame 01](/project-content/project-01-emergence/images/frame-01.jpg)
+
+<video autoplay muted loop>
+  <source src="/project-content/project-01-emergence/videos/frame-02.mp4" type="video/mp4">
+</video>
+
+![Frame 03](/project-content/project-01-emergence/images/frame-03.jpg)
+![Frame 04](/project-content/project-01-emergence/images/frame-04.jpg)
+<!-- ![Frame 05](/project-content/project-01-emergence/images/frame-05.jpg) -->
+![Frame 06](/project-content/project-01-emergence/images/frame-06.jpg)
+
+<video autoplay muted loop>
+  <source src="/project-content/project-01-emergence/videos/frame-07.mp4" type="video/mp4">
+</video>
+
+![Frame 08](/project-content/project-01-emergence/images/frame-08.jpg)
+![Frame 09](/project-content/project-01-emergence/images/frame-09.jpg)
+![Frame 10](/project-content/project-01-emergence/images/frame-10.jpg)
+![Frame 11](/project-content/project-01-emergence/images/frame-11.jpg)
+<!-- ![Frame 12](/project-content/project-01-emergence/images/frame-12.jpg) -->
+
+<video autoplay muted loop>
+  <source src="/project-content/project-01-emergence/videos/frame-13.mp4" type="video/mp4">
+</video>
+
+<!-- ![Frame 14](/project-content/project-01-emergence/images/frame-14.jpg) -->
+![Frame 15](/project-content/project-01-emergence/images/frame-15.jpg)
